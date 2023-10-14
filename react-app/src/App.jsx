2e9fcabfc9e2e4a131ca11f54b336443fb41773f
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-10-14 20:01:48 +0530

    for last project

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -12,7 +12,7 @@ import QR from "./assets/QR-code.png"
 import Email from "./assets/email.png"
 import Phone from "./assets/phone.png"
 import Whatsapp from "./assets/whatsapp.png"
-import Google from "./assets/google.png"
+import Google from "./assets/google-try.png"
 import Gif from "./assets/gear.gif"
 
 function App() {
@@ -33,83 +33,100 @@ React.useEffect(() => {
   const data = [
     {
       id:"1",
-      image:"https://www.kavitsu.com/img/planetary-gearboxes.jpg",
-      name:"Planetary Gearboxes"
+      image:"https://finemanufacturing.com/assets/img/gallery/universaltestingmachine.jpg",
+      name:"Universal Testing Machines"
     },
     {
       id:"2",
-      image:"https://www.kavitsu.com/img/bevel-planetary-speed-reducer.jpg",
-      name:"Bevel Planetary Speed Reducers"
+      image:"https://finemanufacturing.com/assets/img/gallery/acservo.jpg",
+      name:"Electro-Mechanical Servo Controlled Universal Testing Machine"
     },
     {
       id:"3",
-      image:"https://www.kavitsu.com/img/helical-gear-boxes.jpg",
-      name:"Helical Gear Boxes"
+      image:"https://finemanufacturing.com/assets/img/gallery/tensiletestingmachine.jpg",
+      name:"Electro-Mechanical Universal Testing Machine"
     },
     {
       id:"4",
-      image:"https://www.kavitsu.com/img/mini-planetary.jpg",
-      name:"Mini Planetary"
+      image:"https://finemanufacturing.com/assets/img/gallery/impacttestingmachine.jpg",
+      name:"Impact Testing Machine"
     },
     {
       id:"5",
-      image:"https://www.kavitsu.com/img/shaft-mounted-speed-reducer.jpg",
-      name:"Shaft Mounted speed Reducer"
+      image:"https://finemanufacturing.com/assets/img/blog/motobro.jpg",
+      name:"Notch Broaching machine"
     },
     {
       id:"6",
-      image:"https://www.kavitsu.com/img/planetary-creep-drives.jpg",
-      name:"Planetary Creep Drive"
+      image:"https://finemanufacturing.com/assets/img/blog/computarisedbrinell.jpg",
+      name:"Brinell Hardness Testing Machine"
     },
     {
       id:"7",
-      image:"https://www.kavitsu.com/img/planetary-crystilizer.jpg",
-      name:"Planetary Crystallizers"
+      image:"https://finemanufacturing.com/assets/img/blog/analoguehardnesstester1.jpg",
+      name:"Rockwell Hardness Testing Machine"
     },
     {
       id:"8",
-      image:"https://www.kavitsu.com/img/planetary-geared-motor.jpg",
-      name:"Planetary Geared Motor"
+      image:"https://finemanufacturing.com/assets/img/blog/deepthro.jpg",
+      name:"Deep Throat Brinell Hardness Testing Machine"
     },
     {
       id:"9",
-      image:"https://www.kavitsu.com/img/worm-gear-boxes.jpg",
-      name:"worm GEAR Boxes"
+      image:"https://finemanufacturing.com/assets/img/gallery/vickers.jpg",
+      name:"Vickers Hardness Testing Machine"
     },
     {
       id:"10",
-      image:"https://www.kavitsu.com/img/img1.jpg",
-      name:"Planetary Speed Reducer"
+      image:"https://finemanufacturing.com/assets/img/blog/portable11.jpg",
+      name:"Portable Hardness Testing Machines"
     },
     {
       id:"11",
-      image:"https://www.kavitsu.com/img/planetary-sprocket-drives.jpg",
-      name:"Planetary Sprocket Drives"
+      image:"https://finemanufacturing.com/assets/img/blog/barcole.jpg",
+      name:"Barcol Hardness tester"
     },
     {
       id:"12",
-      image:"https://www.kavitsu.com/img/planetary-winches.jpg",
-      name:"Planetary Winches"
+      image:"https://finemanufacturing.com/assets/img/blog/comptorshion.jpg",
+      name:"Digital Torsion Testing Machine"
     },
     {
       id:"13",
-      image:"https://www.kavitsu.com/img/Planetary-Belt-Conveyor-drive.jpg",
-      name:"Planetary Belt Conveyor drive"
+      image:"https://finemanufacturing.com/assets/img/gallery/digitalfatiguetestngmachine.jpg",
+      name:"Fatigue Testing Machine"
     },
     {
       id:"14",
-      image:"https://www.kavitsu.com/img/Special-Multi-Speed-Gear-Boxes-image-for-website.jpg",
-      name:"Special Multi Speed Gear Boxes"
+      image:"https://finemanufacturing.com/assets/img/gallery/compressiontesting.jpg",
+      name:"Digital Compression Testing Machine"
     },
     {
       id:"15",
-      image:"https://www.kavitsu.com/img/Torque-Limiters.jpg",
-      name:"Torque Limiters"
+      image:"https://finemanufacturing.com/assets/img/gallery/set20.jpg",
+      name:"Erichsen Cupping Testing Machine"
     },
     {
       id:"16",
-      image:"https://www.kavitsu.com/img/Planetary-Parallel-axis-&-Uni-Axis.jpg",
-      name:"Planetary Parallel axis & Uni Axis"
+      image:"https://finemanufacturing.com/assets/img/gallery/springtestngmachine.jpg",
+      name:"Spring Testing Machine"
+    },
+    {
+      id:"17",
+      image:"https://finemanufacturing.com/assets/img/gallery/feq251.jpg",
+      name:"End Quench Tester FEQ-25"
+    },
+   
+    {
+      id:"18",
+      image:"https://finemanufacturing.com/assets/img/blog/forcecal.jpg",
+      name:"Force calibration systems"
+    },
+   
+    {
+      id:"19",
+      image:"https://finemanufacturing.com/assets/img/blog/custmain.jpg",
+      name:"Customized Testing Machines"
     },
    
   ]
@@ -122,8 +139,8 @@ React.useEffect(() => {
 {/*  <----------------- NAVBAR -------------------------------> */}
 
       <div className='navbar'>
-          <a href='mailto:marketing@kavitsu.com'><img src={Email} alt="" /><p>marketing@kavitsu.com</p></a>
-          <a href='tel:+919860799510'><img src={Phone} alt="" /><p>+91 9860799510</p></a>
+          <a href='tel:8087572455'><img src={Phone} alt="" /><p>+91 8087572455</p></a>
+          <a href='mailto:response@finemanufacturing.com'><img src={Email} alt="" /><p>response@finemanufacturing.com</p></a>
       </div>
 
       
@@ -135,17 +152,17 @@ React.useEffect(() => {
      
         <div className='icons1'>
           <a href='https://www.linkedin.com/company/kavitsu-transmissions-pvt-ltd/'><img src={Linkedin} alt='' /></a>
-          <a href='https://www.google.com/search?q=kavitsu&rlz=1C5CHFA_enIN1037IN1037&oq=kavitsu+&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgcIARAAGIAEMgcIAhAAGIAEMgcIAxAAGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgyNTUyajBqN6gCALACAA&sourceid=chrome&ie=UTF-8'><img src={Google} alt='' /></a>
           <a href='https://www.instagram.com/kavitsu803/'><img src={Instagram} alt='' /></a>
+          <a href='https://www.google.com/search?q=kavitsu&rlz=1C5CHFA_enIN1037IN1037&oq=kavitsu+&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgcIARAAGIAEMgcIAhAAGIAEMgcIAxAAGIAEMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgyNTUyajBqN6gCALACAA&sourceid=chrome&ie=UTF-8'><img src={Google} alt='' /></a>
         </div>
 
         <div className='logo'>
-        <img src='https://www.kavitsu.com/img/kavitsu-logo1.png' alt='' />
+        <img src='https://finemanufacturing.com/assets/img/logo/logo.png' alt='' />
       </div>
 
         <div className='icons2'>
+        <a href='https://www.facebook.com/KavitsuTransmissions'><img src={Facebook} alt='' /></a>
           <a href='https://www.youtube.com/@kavitsutransmissions7371'><img src={Youtube} alt='' /></a>
-          <a href='https://www.facebook.com/KavitsuTransmissions'><img src={Facebook} alt='' /></a>
           <a href='https://api.whatsapp.com/send?phone=+91%209096469696&text=Hi!%20I%27m%20interested%20in%20your%20service'><img src={Whatsapp} alt='' /></a>
         </div>
       </div>
